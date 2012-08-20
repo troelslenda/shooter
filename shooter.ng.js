d6afd57fbacab1a4d18e8843e08c08cbe82c919f
Christoffer Hallas <christoffer.hallas@forsvikgroup.com>
2012-08-20 12:03:21 +0200

    2 hours work lol

---
+++ shooter.ng.js
@@ -0,0 +1,64 @@
+angular.module('shooter', ['backend'])
+  .config(function () {
+    console.log('onload');
+  })
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', { controller: ShooterCtrl, templateUrl: 'shooter.html' })
+  });
+
+angular.module('backend', ['ngResource'])
+  .factory('Shoot', function ($resource, $filter) {
+    var Shoot = $resource('https://api.mongolab.com/api/1/databases' +
+      '/shooter/collections/shoots/:id', {
+        apiKey: '5031fdb7e4b01966199d1ba2',
+      }, {
+        update: { method: 'PUT' }
+      }
+    );
+    Shoot.prototype.shots = [];
+    Shoot.prototype.getScore = function () {
+      var score = 0;
+      for(i in this.shots) {
+        var shot = this.shots[i];
+        score += (shot.value == 'X' ? 10 : shot.value);
+      }
+      return score;
+    }
+    Shoot.prototype.getBulls = function () {
+      return $filter('filter')(this.shots, { value: 'X' });
+    }
+    return Shoot;
+  });
+
+var Shot = function (value) {
+  this.value = value;
+  this.ignore = '';
+}
+  
+Shot.prototype.ignoreToggle = function () {
+  this.ignore = this.ignore == '' ? 'ignored' : '';
+}
+
+var ShooterCtrl = function ($scope, Shoot) {
+  $scope.shoots = Shoot.query();
+  $scope.values = [0, 5, 6, 7, 8, 9, 10, 'X'];
+
+  $scope.shoot = new Shoot();
+  $scope.shoot.shots = [];
+
+  $scope.saveShoot = function () {
+    $scope.shoot.shooter = $scope.shooter;
+    $scope.shoot.discipline = $scope.discipline;
+    $scope.shoot.timestamp = new Date();
+    $scope.shoot.$save(function () {
+      $scope.shoot = new Shoot();
+      $scope.shoot.shots = [];
+      $scope.shoots = Shoot.query();
+    });
+  }
+
+  $scope.saveShot = function () {
+    $scope.shoot.shots.push(new Shot(this.value));
+  }
+}
