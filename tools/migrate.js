24f043a324033394197e6937ddc73a779a3242b9
Troels Lenda <troelslenda@gmail.com>
2018-09-11 23:39:55 +0200

    Migrate data tool

--- tools/migrate.js
+++ tools/migrate.js
@@ -1,24 +1,50 @@
 var admin = require("firebase-admin");
-const serviceAccount = require('..private.json')
+const serviceAccount = require('../.private/shooter-f5600-firebase-adminsdk-j3sx4-13d4b06b52.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-console.log('hest', process.argv[2]);
+const db = admin.firestore()
 
-let inventory = {};
+db.settings({
+  timestampsInSnapshots: true
+})
 
+const isValidSession = data => !!(
+  data &&
+  data.type &&
+  data.name &&
+  data.time &&
+  data.shots &&
+  data.shots.length > 0
+)
 
-for (i = 1; i <= process.argv[4]; i++) {
-  inventory[i] = {}
+const addSession = async data => {
+  try {
+    const savedSessionRef = await db.collection("sessions").add({
+      name: data.name,
+      ts: data.time
+    })
+    const savedSessionDoc = await savedSessionRef.get()
+    db.collection(`sessions/${savedSessionDoc.id}/round`).add({
+      type: data.type,
+      ts: data.time,
+      shots: data.shots
+    })
+  } catch (e) {
+    console.log(e.message)
+  }
+  finally {
+    console.log(`Session processed for ${data.name}`)
+  }
 }
 
-admin.firestore().collection("stands").doc(process.argv[2]).set({
-    name : `Stand: ${process.argv[2]}`,
-    lastIndexAt: new Date(),
-    requireDeposit: true,
-    standGroup: admin.firestore().doc('standGroups/global'),
-    inventory : inventory,
-    particleId: process.argv[3]
-});
+const migrateData = require('./migratedata.json')
+
+for (i in migrateData) {
+  let row = migrateData[i]
+  if (isValidSession(row)) {
+    addSession(row)
+  }
+}
