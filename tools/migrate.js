1539db2d01ea21ecdbda04ab136bdf981d3a3e0b
Troels Lenda <troelslenda@gmail.com>
2018-09-11 23:51:09 +0200

    Refactoring migration tool and adding comments

--- tools/migrate.js
+++ tools/migrate.js
@@ -1,16 +1,25 @@
+// Load and configure Firebase Admin SDK to facilitate Firestore DB.
 var admin = require("firebase-admin");
 const serviceAccount = require('../.private/shooter-f5600-firebase-adminsdk-j3sx4-13d4b06b52.json');
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-
 const db = admin.firestore()
-
 db.settings({
   timestampsInSnapshots: true
 })
 
+/**
+ * Validate the data in a session
+ * 
+ * Required properties:
+ * - type
+ * - name
+ * - time
+ * - shots
+ * 
+ * @param {*} data 
+ */
 const isValidSession = data => !!(
   data &&
   data.type &&
@@ -20,28 +29,41 @@ const isValidSession = data => !!(
   data.shots.length > 0
 )
 
+/**
+ * Add one session to Firestore
+ * 
+ * @param {*} data 
+ */
 const addSession = async data => {
   try {
+    // Save session and get the refrence to the document.
     const savedSessionRef = await db.collection("sessions").add({
       name: data.name,
-      ts: data.time
+      date: data.time
     })
+    // The refference dosen't directly contain the identifier so
+    // we need to load the document first.
     const savedSessionDoc = await savedSessionRef.get()
+    // Nest the score as a round document under sessions.
+    // This will make further migration easier when grouping
+    // multiple rounds in a session.
     db.collection(`sessions/${savedSessionDoc.id}/round`).add({
       type: data.type,
-      ts: data.time,
+      time: data.time,
       shots: data.shots
     })
   } catch (e) {
     console.log(e.message)
   }
+  // Just log to console to show some kind of progress in the
+  // terminal.
   finally {
     console.log(`Session processed for ${data.name}`)
   }
 }
 
+// Load the data to migrate, loop through, validate and process it.
 const migrateData = require('./migratedata.json')
-
 for (i in migrateData) {
   let row = migrateData[i]
   if (isValidSession(row)) {
